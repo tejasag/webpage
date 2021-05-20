1022bb088ef2e87c9900c5b2eddb6ca747d5901b
Tejas Agarwal <67542663+tejasag@users.noreply.github.com>
2021-05-20 23:42:28 +0530

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,8 +18,8 @@ export default function Home() {
                 <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/devicons/devicon@v2.9.0/devicon.min.css"/>
             </Head>
             <main>
-                <div style={{ padding: "5rem", height: "100vh", backgroundColor: "#2c2c2c", width: "100vw", display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center"}}>
-                    <h1 style={{ fontFamily: "Poppins", color: "#eb6157",  fontSize:"5rem" }}>Website under construction! Please check in later :D</h1>
+                <div style={{  height: "100vh", backgroundColor: "#2c2c2c", width: "100vw", display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center"}}>
+                    <h1 style={{ padding: "5rem", fontFamily: "Poppins", color: "#eb6157",  fontSize:"5rem" }}>Website under construction! Please check in later :D</h1>
                     <span style={{ fontFamily: "Poppins", color: "white", fontSize: "1rem" }}>Last updated: 20th May 2021</span>
                 </div>
             </main>
